d87b3dc217182d0e0b7b7322ae81fc2161d18bc0
Simon <simon.ittensammer@gmx.at>
2019-10-18 09:30:37 +0200

    added fetchVehicleJSON test

--- vehiclest/src/test/java/at/htl/vehicle/rest/VehicleEndpointIT.java
+++ vehiclest/src/test/java/at/htl/vehicle/rest/VehicleEndpointIT.java
@@ -3,6 +3,8 @@ package at.htl.vehicle.rest;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import javax.json.JsonArray;
+import javax.json.JsonObject;
 import javax.ws.rs.client.Client;
 import javax.ws.rs.client.ClientBuilder;
 import javax.ws.rs.client.WebTarget;
@@ -10,7 +12,7 @@ import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
 import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.*;
 
 public class VehicleEndpointIT {
     private Client client;
@@ -28,5 +30,14 @@ public class VehicleEndpointIT {
         assertThat(response.getStatus(), is(200));
         String payload = response.readEntity(String.class);
         System.out.println("payload = " + payload);
+    }
+
+    @Test
+    public void fetchVehicleJSON() {
+        Response response = (Response) this.target.request(MediaType.APPLICATION_JSON).get();
+        JsonArray allTodos = response.readEntity(JsonArray.class);
+        JsonObject vehicle = allTodos.getJsonObject(0);
+        assertThat(vehicle.getString("brand"), equalTo("Opel 42"));
+        assertThat(vehicle.getString("type"), startsWith("Commodore"));
     }
 }
